559e47e0207392aef848ff4bebd7402c705e2745
styxpilled <styxpilled.dev@gmail.com>
2023-01-15 01:24:03 +0100

    refactor: match mode instead of event

--- src/main.rs
+++ src/main.rs
@@ -31,91 +31,103 @@ enum Mode {
     Command,
 }
 
-fn draw(event: Event, stdo: &mut Stdout, brush: &mut char, mode: &mut Mode) -> bool {
+fn draw(event: Event, stdout: &mut Stdout, brush: &mut char, mode: &mut Mode) -> bool {
     if event == Event::Key(KeyCode::Esc.into()) {
         if *mode == Mode::Command {
             return false;
         }
+        execute!(stdout, cursor::Hide).unwrap();
         *mode = Mode::Command;
     }
-    match event {
-        Event::Mouse(ev) => {
-            if *mode == Mode::Draw {
-                match ev.kind {
-                    MouseEventKind::Drag(_) | MouseEventKind::Down(_) => {
+
+    match *mode {
+        Mode::Command => match event {
+            Event::Key(ev) => match ev.code {
+                KeyCode::Char(code) => {
+                    *mode = match code {
+                        'i' => {
+                            execute!(stdout, cursor::Show).unwrap();
+                            Mode::Insert
+                        }
+                        'd' => {
+                            execute!(stdout, cursor::Hide).unwrap();
+                            Mode::Draw
+                        }
+                        'q' => {
+                            execute!(stdout, Clear(ClearType::All)).unwrap();
+                            Mode::Command
+                        }
+                        _ => *mode,
+                    }
+                }
+                _ => {}
+            },
+            _ => {}
+        },
+        Mode::Insert => {
+            match event {
+                Event::Key(ev) => match ev.code {
+                    KeyCode::Char(code) => {
+                        execute!(stdout, crossterm::style::Print(code),).unwrap();
+                    }
+                    KeyCode::Left => {
+                        execute!(stdout, cursor::MoveLeft(1)).unwrap();
+                    }
+                    KeyCode::Right => {
+                        execute!(stdout, cursor::MoveRight(1)).unwrap();
+                    }
+                    KeyCode::Up => {
+                        execute!(stdout, cursor::MoveUp(1)).unwrap();
+                    }
+                    KeyCode::Down => {
+                        execute!(stdout, cursor::MoveDown(1)).unwrap();
+                    }
+                    KeyCode::Backspace => {
                         execute!(
-                            stdo,
-                            cursor::MoveTo(ev.column, ev.row),
-                            crossterm::style::Print(*brush)
+                            stdout,
+                            cursor::MoveLeft(1),
+                            crossterm::style::Print(" "),
+                            cursor::MoveLeft(1)
                         )
                         .unwrap();
                     }
                     _ => {}
+                },
+                _ => {}
+            };
+        }
+        Mode::Draw => match event {
+            Event::Mouse(ev) => {
+                if *mode == Mode::Draw {
+                    match ev.kind {
+                        MouseEventKind::Drag(_) | MouseEventKind::Down(_) => {
+                            execute!(
+                                stdout,
+                                cursor::MoveTo(ev.column, ev.row),
+                                crossterm::style::Print(*brush)
+                            )
+                            .unwrap();
+                        }
+                        _ => {}
+                    }
                 }
             }
-        }
-        Event::Key(ev) => {
-            // if ev.modifiers == KeyModifiers::SHIFT {
-            //     print!("{:?}", ev);
-            // }
-            match ev.code {
+            Event::Key(ev) => match ev.code {
                 KeyCode::Char(code) => {
-                    match *mode {
-                        Mode::Command => {
-                            *mode = match code {
-                                'i' => {
-                                    execute!(stdo, cursor::Show).unwrap();
-                                    Mode::Insert
-                                }
-                                'd' => {
-                                    execute!(stdo, cursor::Hide).unwrap();
-                                    Mode::Draw
-                                }
-                                'q' => {
-                                    execute!(stdo, Clear(ClearType::All)).unwrap();
-                                    Mode::Command
-                                }
-                                _ => *mode,
-                            }
-                        }
-                        Mode::Insert => {
-                            execute!(stdo, crossterm::style::Print(code),).unwrap();
-                        }
-                        Mode::Draw => {
-                            *brush = code;
-                        }
-                    };
-                }
-                KeyCode::Left => {
-                    execute!(stdo, cursor::MoveLeft(1)).unwrap();
-                }
-                KeyCode::Right => {
-                    execute!(stdo, cursor::MoveRight(1)).unwrap();
-                }
-                KeyCode::Up => {
-                    execute!(stdo, cursor::MoveUp(1)).unwrap();
-                }
-                KeyCode::Down => {
-                    execute!(stdo, cursor::MoveDown(1)).unwrap();
-                }
-                //  | KeyCode::Up | KeyCode::Down
-                KeyCode::Backspace => {
-                    execute!(
-                        stdo,
-                        cursor::MoveLeft(1),
-                        crossterm::style::Print(" "),
-                        cursor::MoveLeft(1)
-                    )
-                    .unwrap();
+                    *brush = code;
                 }
                 _ => {}
-            }
-        }
-        _ => {}
-    };
+            },
+            _ => {}
+        },
+    }
+    // if ev.modifiers == KeyModifiers::SHIFT {
+    //     print!("{:?}", ev);
+    // }
+
     let (x, y) = position().unwrap_or_default();
     let (max_x, googa) = size().unwrap_or_default();
-    execute!(stdo, cursor::MoveTo(0, googa)).unwrap();
+    execute!(stdout, cursor::MoveTo(0, googa)).unwrap();
     let mode_text = match *mode {
         Mode::Command => "COMMAND",
         Mode::Insert => "INSERT",
@@ -125,7 +137,7 @@ fn draw(event: Event, stdo: &mut Stdout, brush: &mut char, mode: &mut Mode) -> b
         "{mode_text} MODE, pos: ({x}, {y}), max_pos: ({max_x}, {googa}), brush: {}",
         brush.clone()
     );
-    execute!(stdo, cursor::MoveTo(x, y)).unwrap();
+    execute!(stdout, cursor::MoveTo(x, y)).unwrap();
     true
 }
 
@@ -137,14 +149,14 @@ async fn event_handler() {
     loop {
         let mut delay = Delay::new(Duration::from_millis(1_000)).fuse();
         let mut event = reader.next().fuse();
-        let mut stdo = stdout();
+        let mut stdout = stdout();
 
         select! {
             _ = delay => {  },
             maybe_event = event => {
                 match maybe_event {
                     Some(Ok(event)) => {
-                        match draw(event, &mut stdo, &mut brush, &mut mode) {
+                        match draw(event, &mut stdout, &mut brush, &mut mode) {
                             false => break,
                             true => {}
                         };
