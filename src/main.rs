7bba3f3fb10eccd4a5d09b6163f8a2d9521da653
styxpilled <styxpilled.dev@gmail.com>
2023-02-12 01:05:21 +0100

    fix: crashes on drawing on bound line

--- src/main.rs
+++ src/main.rs
@@ -294,23 +294,22 @@ fn draw(event: Event, stdout: &mut Stdout, state: &mut State, colors: &Vec<Color
                 | MouseEventKind::Down(MouseButton::Left) => {
                     let (x, y) = (ev.column, ev.row);
                     let mut average_luma = 0;
-                    let (max_x, max_y) = terminal::size().unwrap_or_default();
+                    let (mx, my) = terminal::size().unwrap_or_default();
                     let mut divider = 0;
-                    for n in (x - 1)..(x + 2) {
-                        if n > 1 && n < max_x {
-                            for i in (y - 1)..(y + 2) {
-                                if i > 1 && i < max_y {
-                                    divider += 1;
-                                    average_luma += LUMA_VALUES
-                                        .iter()
-                                        .position(|&val| {
-                                            val == state.virtual_display[usize::from(n)]
-                                                [usize::from(i)]
-                                            .brush
-                                        })
-                                        .unwrap_or(50);
-                                }
-                            }
+                    let col_range =
+                        if x > 1 { x - 1 } else { x }..if x + 1 < mx { x + 1 } else { x } + 1;
+                    let row_range =
+                        if y > 1 { y - 1 } else { y }..if y + 1 < my { y + 1 } else { y } + 1;
+                    for n in col_range {
+                        for i in row_range.clone() {
+                            divider += 1;
+                            average_luma += LUMA_VALUES
+                                .iter()
+                                .position(|&val| {
+                                    val == state.virtual_display[usize::from(n)][usize::from(i)]
+                                        .brush
+                                })
+                                .unwrap_or(50);
                         }
                     }
                     average_luma = average_luma / divider;
